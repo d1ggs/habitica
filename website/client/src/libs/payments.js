648070bd6281a8d048eb15e3c711c814a48aca07
Adrian Winterstein <adrian@winterstein.biz>
2026-08-30 17:22:02 +0200

    Disables analytics.

--- website/client/src/libs/payments.js
+++ website/client/src/libs/payments.js
@@ -1,7 +1,9 @@
 export function setup () { // eslint-disable-line import/prefer-default-export
+  /*
   const stripeScript = document.createElement('script');
   const firstScript = document.getElementsByTagName('script')[0];
   stripeScript.async = true;
   stripeScript.src = 'https://js.stripe.com/v3/';
   firstScript.parentNode.insertBefore(stripeScript, firstScript);
+  */
 }
--- website/server/libs/payments/paypal.js
+++ website/server/libs/payments/paypal.js
@@ -1,4 +1,6 @@
 /* eslint-disable camelcase */
+/* eslint-disable max-len */
+/*
 import nconf from 'nconf';
 import moment from 'moment';
 import util from 'util';
@@ -279,6 +281,7 @@ api.getSubscriptionPaymentDetails = async function getSubscriptionPaymentDetails
  *
  * @return undefined
  */
+/*
 api.subscribeCancel = async function subscribeCancel (options = {}) {
   const { groupId, user, cancellationReason } = options;
 
@@ -387,3 +390,4 @@ api.ipn = async function ipnApi (options = {}) {
 };
 
 export default api;
+*/
